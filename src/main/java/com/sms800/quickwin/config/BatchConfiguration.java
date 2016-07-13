59184383e9ccce6697c9454a4b3e1b28497f8d18
Soumen Choudhury <sou.tcs@gmail.com>
2016-07-13 16:46:53 +0530

    Update code

--- src/main/java/com/sms800/quickwin/config/BatchConfiguration.java
+++ src/main/java/com/sms800/quickwin/config/BatchConfiguration.java
@@ -1,5 +1,8 @@
 package com.sms800.quickwin.config;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.sql.DataSource;
 
 import org.springframework.batch.core.Job;
@@ -12,14 +15,13 @@ import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
-import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
-import org.springframework.batch.item.file.mapping.DefaultLineMapper;
-import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import com.sms800.quickwin.batch.Marksheet;
@@ -30,8 +32,8 @@ import com.sms800.quickwin.batch.Student;
 public class BatchConfiguration {
 
     @Bean
-    public ItemReader<Student> reader() {
-        FlatFileItemReader<Student> reader = new FlatFileItemReader<Student>();
+    public ItemReader<Student> reader(@Qualifier("dataSourceqWH") DataSource dataSource) {
+        /*FlatFileItemReader<Student> reader = new FlatFileItemReader<Student>();
         reader.setResource(new ClassPathResource("student-data.csv"));
         reader.setLineMapper(new DefaultLineMapper<Student>() {{
             setLineTokenizer(new DelimitedLineTokenizer() {{
@@ -39,14 +41,30 @@ public class BatchConfiguration {
             }});
             setFieldSetMapper(new BeanWrapperFieldSetMapper<Student>() {{
                 setTargetType(Student.class);
-            }});
-        }});
-        return reader;
+            }});	
+        }});*/
+    	//return reader;
+        
+        JdbcCursorItemReader<Student> databaseReader = new JdbcCursorItemReader<>();
+        databaseReader.setDataSource(dataSource);
+        databaseReader.setSql("select areacode, areacode from areacode LIMIT 10 ");
+        databaseReader.setRowMapper(new RowMapper<Student>() {
+			@Override
+			public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Student stu=new Student();
+				stu.setStdId(rs.getString("areacode"));
+				stu.setSubMarkOne(rs.getInt("areacode"));
+				
+				return stu;
+			}
+		});
+		
+        return databaseReader;
+        
     }
    
-
-    @Bean
-    public ItemWriter<Marksheet> writer(DataSource dataSource) {
+    @Bean  
+    public ItemWriter<Marksheet> writer(@Qualifier("dataSourceqQW") DataSource dataSource) {
         JdbcBatchItemWriter<Marksheet> writer = new JdbcBatchItemWriter<Marksheet>();
         writer.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<Marksheet>());
         writer.setSql("INSERT INTO marksheet (studentId,totalMark) VALUES (:stdId,:totalSubMark)");
@@ -79,15 +97,34 @@ public class BatchConfiguration {
     }
 
     @Bean
-    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
-        return new JdbcTemplate(dataSource);
+    @Qualifier("dataSourceqQW")
+    @Primary
+    public JdbcTemplate jdbcTemplateQW(DataSource dataSource) {
+        return new JdbcTemplate(getDataSourceQW());
     }
     
     @Bean
-	public DataSource getDataSource() {
+    @Qualifier("dataSourceqWH")
+    public JdbcTemplate jdbcTemplateWH(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+    
+    @Bean(name="dataSourceqQW")
+    @Primary
+	public DataSource getDataSourceQW() {
     	DriverManagerDataSource dataSource = new DriverManagerDataSource();
-	    dataSource.setDriverClassName("com.mysql.jdbc.Driver");	//org.postgresql.Driver");
+	    dataSource.setDriverClassName("com.mysql.jdbc.Driver");	
 	    dataSource.setUrl("jdbc:mysql://localhost:3306/springbatch?characterEncoding=UTF-8&useSSL=false");
+	    dataSource.setUsername("root");
+	    dataSource.setPassword("password");
+	    return dataSource;
+	}
+    
+    @Bean(name="dataSourceqWH")
+	public DataSource getDataSourceWH() {
+    	DriverManagerDataSource dataSource = new DriverManagerDataSource();
+	    dataSource.setDriverClassName("com.mysql.jdbc.Driver");	
+	    dataSource.setUrl("jdbc:mysql://localhost:3306/dbmigration?characterEncoding=UTF-8&useSSL=false");
 	    dataSource.setUsername("root");
 	    dataSource.setPassword("password");
 	    return dataSource;
--- src/main/java/com/sms800/quickwin/config/DbMigraionBatchApplication.java
+++ src/main/java/com/sms800/quickwin/config/DbMigraionBatchApplication.java
@@ -4,11 +4,17 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
+import javax.sql.DataSource;
+
+import org.springframework.batch.core.configuration.annotation.BatchConfigurer;
+import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -20,8 +26,18 @@ import com.sms800.quickwin.batch.Marksheet;
 @Import({BatchConfiguration.class})
 public class DbMigraionBatchApplication implements CommandLineRunner{
 
+	@Bean
+	BatchConfigurer configurer(@Qualifier("dataSourceqQW")DataSource dataSource){
+	  return new DefaultBatchConfigurer(dataSource);
+	}
+	
 	@Autowired
-	JdbcTemplate jdbcTemplate;
+	@Qualifier("dataSourceqWH")
+	JdbcTemplate dataSourceqWH;
+	
+	@Autowired
+	@Qualifier("dataSourceqQW")
+	JdbcTemplate dataSourceqQW;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(DbMigraionBatchApplication.class, args);
@@ -30,14 +46,13 @@ public class DbMigraionBatchApplication implements CommandLineRunner{
 	
     @Override
 	public void run(String... arg0) throws Exception {
-    	List<Marksheet> result = jdbcTemplate.query("select studentId,totalMark FROM marksheet", 
-        		new RowMapper<Marksheet>() {
+    	/*List<Marksheet> result = dataSourceqWH.query("select studentId,totalMark FROM marksheet", new RowMapper<Marksheet>() {
             @Override
             public Marksheet mapRow(ResultSet rs, int row) throws SQLException {
                 return new Marksheet(rs.getString(1), Integer.parseInt(rs.getString(2)));
             }
         });
-        System.out.println("Number of Record:"+result.size());
+        System.out.println("Number of Record:"+result.size());*/
 	}
 }
 
